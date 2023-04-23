8738cf327cd0217ab20eef2a1349e40305cf34b4
Emir Balcı <emirbalcii96@gmail.com>
2023-04-23 15:26:37 +0300

    Mail entegrasyonu yapıldı.

--- ArchitectureBlog.UI/Controllers/HomeController.cs
+++ ArchitectureBlog.UI/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MailKit.Security;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace ArchitectureBlog.UI.Controllers
 {
@@ -8,10 +12,37 @@ namespace ArchitectureBlog.UI.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult SendMail(string contact_name, string contact_email, string contact_message)
+        {
+            List<InternetAddress> a = new List<InternetAddress>
+            {
+                MailboxAddress.Parse("emir.balci@hangikredi.com"),
+                MailboxAddress.Parse("muhammedyucedagg@gmail.com")
+            };
+
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("emirbalcii96@gmail.com"));
+            email.To.AddRange(a);
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = "<h1>Example HTML Message Body</h1>" };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, false);
+            smtp.Authenticate("emirbalcii96@gmail.com", "rttbytlreqzqxqix");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return View();
+        }
+
         public IActionResult ArchitectureBlog()
         {
             return View();
         }
+
         public IActionResult ArchitectureBlogDetail()
         {
             return View();
